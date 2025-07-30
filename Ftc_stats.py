b95241c53970af24a97d37a46abb1c92decb77c3
alexanton <alex.anton756@gmail.com>
2025-07-30 23:16:04 +0300

    More Options

--- Ftc_stats.py
+++ Ftc_stats.py
@@ -1,16 +1,40 @@
 import pandas as pd
 import requests
 
-year = input("Enter year: ")
-team_id = input("Enter team id: ")
 
-url = f"https://api.ftcscout.org/rest/v1/teams/{team_id}/events/{year}"
+option = input("1 - All Event Stats\n"
+               "2 - Team Stats\n"
+               "Enter option: ")
 
-response = requests.get(url)
-data = response.json()
+if option == "1":
+    team_id = input("Enter team ID: ")
+    year = input("Enter year: ")
+    url1 = f"https://api.ftcscout.org/rest/v1/teams/{team_id}/events/{year}"
+    response = requests.get(url1)
 
-df = pd.DataFrame(data)
-df = pd.json_normalize(data)
+    data = response.json()
 
-excel = "TeamStats.xlsx"
-df.to_excel(excel, index=False)
+    df = pd.DataFrame(data)
+    df = pd.json_normalize(data)
+    pd.set_option('display.max_columns', None)
+    df = df[['stats.avg.totalPoints', 'stats.avg.autoPoints', 'stats.avg.dcPoints', 'stats.rank', 'eventCode']]
+    df = df.rename(columns={'stats.avg.totalPoints': 'Total', 'stats.avg.autoPoints': 'Auto Avg', 'stats.avg.dcPoints': 'DC Avg', 'stats.rank': 'Rank', 'eventCode': 'Event'})
+
+    print(df)
+
+
+if option == "2":
+    team_id = input("Enter team ID: ")
+    url2 = f"https://api.ftcscout.org/rest/v1/teams/{team_id}/quick-stats"
+    response = requests.get(url2)
+    data = response.json()
+
+    df = pd.DataFrame(data)
+    df = pd.json_normalize(data)
+    pd.set_option('display.max_columns', None)
+    print(df)
+
+
+
+#excel = "TeamStats.xlsx"
+#df.to_excel(excel, index=False)
